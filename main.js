56de28ba2c9b72a68c1b0904f4810abe09fea438
killtothereal <krakus540@gmail.com>
2024-01-10 10:34:18 +0300

    traffic and collisions

--- main.js
+++ main.js
@@ -4,19 +4,26 @@ canvas.width = 200;
 //Context
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width / 2, canvas.width * 0.95);
-const car = new Car(road.getLaneCenter(1), 100, 30, 50);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS");
 
+const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2)];
 animate();
 
 function animate() {
-  car.update(road.borders);
+  for (let i = 0; i < traffic.length; i++) {
+    traffic[i].update(road.borders, []);
+  }
+  car.update(road.borders, traffic);
   // Resizing canvas makes it clear
   canvas.height = window.innerHeight;
   ctx.save();
   //Fixing camera on a car
   ctx.translate(0, -car.y + canvas.height * 0.7);
   road.draw(ctx);
-  car.draw(ctx);
+  for (let i = 0; i < traffic.length; i++) {
+    traffic[i].draw(ctx, "red");
+  }
+  car.draw(ctx, "blue");
   ctx.restore();
   //Recalling animate function
   requestAnimationFrame(animate);
